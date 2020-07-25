014fe26b32e7928e907ab83d24c80c05368e9a25
lkuehnl1 <35829651+lkuehnl1@users.noreply.github.com>
2020-07-25 15:17:17 -0500

    Add project files.

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Encounter with Destiny!");
+            Console.WriteLine("This will be an amazing adventure " +
+                "that will \nchallenge you to the core of your being");
+            Console.WriteLine("Please enter your name");
+            string CharacterName = Console.ReadLine();
+            
+            Console.WriteLine("Your name is " + CharacterName.ToUpper() + ". Phenomenal!");
+            Console.ReadKey();
+
+           
+        }
+    }
+}
